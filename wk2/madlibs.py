6355b680825d2239abc9a58799f645767ae7fb0d
TrentonMay <TrentonMay@fullsail.edu>
2015-11-30 17:50:19 -0500

    Added comments

--- wk2/madlibs.py
+++ wk2/madlibs.py
@@ -1,4 +1,6 @@
 print "Before your story begins"
+
+#Below lines are for where the user will enter their information
 knight_name = raw_input("Enter your Knightly Name ")
 land_name = raw_input("Enter the name of your medieval country ")
 king_name = raw_input("Enter the name of your King ")
@@ -9,21 +11,22 @@ num1 = raw_input("Enter a number between 0-5 ")
 num2 = raw_input("Enter a number between 5-10 ")
 num3 = raw_input("Enter a number between 10-17 ")
 
+#These two lines allow me to generate a random number to be used later (in the conclusion)
 import random
 num1_rand = num1 * random.randrange(1, 3)
 
+#These lines are for me to store information in arrays and dictionaries to be used later. These are the town lists
 town_arr = ["Ghanshire", "Steadholme", "Ravenwood", "Riveton", "Alefast", "Nahrsong"]
 town_dict = dict()
 town_dict = {"Skyrim":"Dawnstar", "LOTR":"Archet", "GOT":"Winterfell", "Oblivion":"Cyrodil", "ESO":"Mathiisen", "Vikings":"Kattegat"}
 
-print town_dict["Skyrim"]
-
-def calconclusion(a, b):
+#This function uses the information the user inputs to calculate a result. This result is used to decide that the ending is
+def conclusion(a, b):
     final = a + b
     return final
-c = calconclusion(int(num1), int(num3))
-print c
+c = conclusion(int(num1), int(num3))
 
+#This is the first leg of the story. it's basic in structure but it gives the basis for what happens, using what the user inputted
 print "Your Story begins!"
 print "You are a Knight in the land of " + str(land_name) + ". Your name is " + str(knight_name) + " and you " \
       "serve the king of " + str(king_name) + ". A great Red Dragon by the name of Bahlgore is wreaking havoc " \
@@ -41,6 +44,7 @@ print "You are a Knight in the land of " + str(land_name) + ". Your name is " + 
       "sort of deep humming as if there was someone with a deep voice singing a song. As you move deeper into the den you " \
       "start to make out the lyrics to the song. "
 
+#below is the song the dragon sings. Instead of inputting repeated words I used multiple loops (I don't know how to do it all in one yet) to say what the dragon says
 print "Up in the sky I see down below"
 for i in range(0,3):
     print "Below"
@@ -59,6 +63,7 @@ for i in range(0,3):
 
 print "Bahlgore then begins to name the towns he has burned only to ignite the hatred in your heart even more"
 
+#Here is where the towns in the land are decided upon what number the user chose
 if int(num1) > 3:
     print town_arr[0]
     print town_arr[1]
@@ -74,6 +79,7 @@ else:
     print town_dict["ESO"]
     print town_dict["Vikings"]
 
+#This is more story. It's basically the ending but this section is strictly just for the story to move forward and help end
 print "His deep voice sends chills down your spine but you move forward anyways to as far as you an go without being in sight of the " \
       "beast. You see the beast and how he towers above you like a giant. Even though you are trembling with fear you spot where he keeps " \
       "his pile of fresh meat for eating and place the enchanted" + str(f_animal) + "meat in the pile. You wait in a corner for what " \
@@ -83,6 +89,7 @@ print "His deep voice sends chills down your spine but you move forward anyways 
       "better shot at killing the wretched beast so you move in and slice off his head with one clean swipe of your sword. Immediately you travel " \
       "back to the kingdom to claim your reward and your honor"
 
+#This is the if statement that uses the function above to decide how the story should end. it also uses a randomly generated number for the gold pieces
 if c > 20:
     print "Upon your arrival you see that the entire kingdom was burned by the hands of Bahlgore whilst you we're on your journey to defeat him"
 else:
