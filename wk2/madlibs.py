9de148f69179526ee18b68c8abf94eaa2a5bd33f
TrentonMay <TrentonMay@fullsail.edu>
2015-11-30 15:35:26 -0500

    Doing some testing on random numbers

--- wk2/madlibs.py
+++ wk2/madlibs.py
@@ -6,3 +6,9 @@ f_animal = raw_input("What's your favorite animal?")
 f_jewelry = raw_input("What's your favorite piece of jewelry")
 num1 = raw_input("Enter a number between 0-5")
 num2 = raw_input("Enter a number between 5-10")
+
+num1_rand = num1 * randrange(2, 8)
+num2_rand = num2 * randrange(1, 10)
+
+print num1_rand
+print num2_rand
