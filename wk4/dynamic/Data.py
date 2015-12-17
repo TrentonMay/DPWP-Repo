bcc9d2d9e44e529bcbb2af34a7daf477c420044e
TrentonMay <TrentonMay@fullsail.edu>
2015-12-17 18:19:22 -0500

    Commented Data.py

--- wk4/dynamic/Data.py
+++ wk4/dynamic/Data.py
@@ -1,18 +1,26 @@
+"""
+This is the python file where all of the hard coded data is placed
+See comments below for more details on each attribute, function, and class
+When "Below" is used in the comment it is speaking of the line directly below
+"""
+# Below, this is the class that will store values from the class below it that inherits from it
+# All data is not private so that there can be an openess of information
 class AnimeData(object):
-    def __init__(self):
-        self.title = ""
-        self.image = ""
-        self.writer = ""
-        self.publisher = ""
-        self.director = ""
-        self.episodes = 0
-        self.desc = ""
+    def __init__(self):     # Initializing
+        self.title = ""     # Attribute for the title of the anime
+        self.image = ""     # Attribute for the image file for the style of the anime
+        self.writer = ""        # Attribute for the write of the anime
+        self.publisher = ""     # Attribute for the publisher of the anime
+        self.director = ""      # Attribute for the director of the anime
+        self.episodes = 0       # Attribute for the episodes of the anime
+        self.desc = ""      # Attribute for the description of the anime
 
+# Below, this is the Anime class which holds the hard coded data. This inherits from the AnimeData class
 class Anime(AnimeData):
-    def __init__(self):
-        super(AnimeData, self).__init__()
+    def __init__(self):     # Initializing
+        super(AnimeData, self).__init__()   # Constructor function so that the attributes from the super can carry over
 
-        sao = AnimeData()
+        sao = AnimeData()   # Sword Art Online instance
         sao.title = "Sword Art Online"
         sao.image = "css/sao_d.jpg"
         sao.writer = " Reki Kawahara"
@@ -29,7 +37,7 @@ class Anime(AnimeData):
                    "game means death in the real world. Now, with no one else to turn to, Kirito and the other " \
                    "participants must survive the game as best they can."
 
-        dbz = AnimeData()
+        dbz = AnimeData()   # Dragon Ball Z instance
         dbz.title = "Dragon Ball Z"
         dbz.image = "css/dbz_d.jpg"
         dbz.writer = " Takao Koyama"
@@ -40,7 +48,7 @@ class Anime(AnimeData):
                    "from the darkest corners of space. Joined in battle by the Z-Fighters, Goku travels to distant " \
                    "realms in search of the magic powers of the seven Dragon Balls!"
 
-        aot = AnimeData()
+        aot = AnimeData()   # Attack On Titan instance
         aot.title = "Attack On Titan"
         aot.image = "css/aot_d.jpg"
         aot.writer = " Yasuko Kobayashi"
@@ -55,7 +63,7 @@ class Anime(AnimeData):
                    "a peaceful existence until the day that dream was shattered, and their slim chance at survival " \
                    "was reduced to one horrifying choice: kill - or be devoured!"
 
-        rk = AnimeData()
+        rk = AnimeData()    # Rurouni Kenshin instance
         rk.title = "Rurouni Kenshin"
         rk.image = "css/rk_d.jpg"
         rk.writer = " Mari Okada"
@@ -69,7 +77,7 @@ class Anime(AnimeData):
                   'gives up the life of "Battousai The Man Slayer" and sets off as a lone wanderer. His travels lead ' \
                   'to the Kamiya Dojo where he discovers the chance to start life over'
 
-        naruto = AnimeData()
+        naruto = AnimeData()    # naruto instance
         naruto.title = "Naruto"
         naruto.image = "css/naruto_d.jpg"
         naruto.writer = " Masashi Kishimoto"
@@ -81,5 +89,6 @@ class Anime(AnimeData):
                       "a boy in exchange for his own life. Naruto was that boy, and he grew up with no family, and " \
                       "the villagers hated him thinking that he himself was the demon fox. Naruto's dream is to become " \
                       "Hokage, and have the villagers acknowledge him"
+        # Below, storing the instances above in an array so that they may be easily accessed in other files
         self.anime = [sao, dbz, aot, rk, naruto]
 
